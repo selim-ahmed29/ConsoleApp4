ccfcd6ed1fd23b516491dde667e21bb50b585e6f
Selim Ahmed <selimahmed0329@gmail.com>
2025-06-27 00:41:34 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,220 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            //Console.WriteLine("enter a number to see if it is divisble by 3 & 4 or not");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 3 == 0 && num % 4 == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+            #endregion
+
+            #region Question 2
+            //Console.WriteLine("enter a number to see is it negative or positive");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num < 0)
+            //    Console.WriteLine("negative");
+            //else
+            //    Console.WriteLine("positive");
+            #endregion
+
+            #region Question 3
+            //Console.WriteLine("Enter first number:");
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number:");
+            //int y = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter third number:");
+            //int z = int.Parse(Console.ReadLine());
+
+            //int max = x;
+            //if (y>max) max = y;
+            //if (z>max) max = z;
+
+            //int min = x;
+            //if (y < min) min = y;
+            //if (z < min) min = z;
+
+            //Console.WriteLine("Max element = " + max);
+            //Console.WriteLine("Min element = " + min);
+            #endregion
+
+            #region Question 4
+            //Console.WriteLine("enter a number");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //    Console.WriteLine("the number is Even");
+            //else
+            //    Console.WriteLine("the number is Odd");
+            #endregion
+
+            #region Question 5
+            //Console.WriteLine("write a litter ");
+            //char litter = char.Parse(Console.ReadLine());
+            //if ("aeiouAEIOU".Contains(litter))
+            //    Console.WriteLine("Vowel");
+            //else
+            //    Console.WriteLine("Consonant");
+            #endregion
+
+            #region Question 6
+            //Console.WriteLine("enter a number");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //    Console.Write(i +",");
+            #endregion
+
+            #region Question 7
+            //Console.WriteLine("enter a number from 1 to 12");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //    Console.Write(i*n+ ",");
+            #endregion
+
+            #region Question 8
+            //Console.WriteLine("enter a number");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //    if (i % 2 == 0)
+            //        Console.Write(i+",");
+            #endregion
+
+            #region Question 9
+            //Console.WriteLine("Enter the number:");
+            //int number = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the power number:");
+            //int power = int.Parse(Console.ReadLine());
+
+            //int answer = 1;
+
+            //for (int i = 1; i <= power; i++)
+            //    answer *= number;
+
+            //Console.WriteLine(answer);
+            #endregion
+
+            #region Question 10
+            //Console.WriteLine("Enter five marks:");
+            //int[] marks = new int[5];
+            //int sum = 0;
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    marks[i] = int.Parse(Console.ReadLine());
+            //    sum += marks[i];
+            //}
+            //double avg = sum / marks.Length;
+            //double percent = avg ;
+            //Console.WriteLine("Total marks = " + sum);
+            //Console.WriteLine("Average Marks = " + avg);
+            //Console.WriteLine("Percentage = " + percent);
+            #endregion
+
+            #region Question 11
+            //Console.WriteLine("Enter the month number:");
+            //int month = int.Parse(Console.ReadLine());
+
+            //if (month == 2)
+            //    Console.WriteLine("28 or 29 days");
+            //else if (month == 4 || month == 6 || month == 9 || month == 11)
+            //    Console.WriteLine("30 days");
+            //else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            //    Console.WriteLine("31 days");
+            //else
+            //    Console.WriteLine("Invalid month");
+            #endregion
+
+            #region Question 12
+            //int n = int.Parse(Console.ReadLine());
+
+            #endregion
+
+            #region Question 13
+            //Console.WriteLine("Enter five numbers:");
+            //int[] arr = new int[5];
+            //int sum = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //    sum += arr[i];
+            //}
+
+            //Console.WriteLine("Sum = " + sum);
+            #endregion
+
+            #region Question 14
+
+            #endregion
+
+            #region Question 15
+            //Console.WriteLine("Enter five numbers:");
+            //int[] arr = new int[6];
+            //int max = arr[0];
+            //int min = arr[0];
+
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //    if (arr[i] > max) max = arr[i];
+            //    if (arr[i] < min) min = arr[i];
+            //}
+
+            //Console.WriteLine("max = " + max);
+            //Console.WriteLine("min = " + min);
+            ////have logic error '_'
+            #endregion
+
+            #region Question 15
+            //Console.WriteLine("Enter five numbers:");
+            //int[] arr = new int[5];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int max = arr[0];
+            //int min = arr[0];
+
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max) max = arr[i];
+            //    if (arr[i] < min) min = arr[i];
+            //}
+
+            //Console.WriteLine("max = " + max);
+            //Console.WriteLine("min = " + min);
+            #endregion
+
+            #region Question 16
+            //Console.WriteLine("Enter five numbers:");
+            //int[] arr = new int[5];
+            //int max=0;
+            //int secondMax=0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //    if (arr[i] > max)
+            //    {
+            //        secondMax = max;
+            //        max = arr[i];
+            //    }
+            //    else if (arr[i] > secondMax && arr[i] != max)
+            //    {
+            //        secondMax = arr[i];
+            //    }
+            //}
+
+            //Console.WriteLine("Second Largest = " + secondMax);
+            #endregion
+
+        }
+    }
+}
